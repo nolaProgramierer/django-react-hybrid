d65b11da801af7da877798fa502a2fd21dfc8d8c
Glenn Langdon <glennlangdon@gmail.com>
2023-08-01 22:49:09 -0500

    piano view

--- django_hybrid/piano/views.py
+++ django_hybrid/piano/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Piano, Comment
+
+
 def index(request):
+    pianos = Piano.objects.all()
     return render(request, "piano/index.html")
